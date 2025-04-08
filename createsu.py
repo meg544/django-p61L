39b99dffc4edc9294973a56eabeb13df13070352
mescobedo <meg_544@yahoo.com.mx>
2025-04-07 21:35:56 -0600

    crea superuser

---
+++ createsu.py
@@ -0,0 +1,20 @@
+import os
+import django
+
+# Configurar entorno Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+email = "meg_544@yahoo.com.mx"
+password = "rabanito"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("✅ Superusuario creado con éxito.")
+else:
+    print("⚠️ El superusuario ya existe.")
