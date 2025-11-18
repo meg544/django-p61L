c50c7b9da171230d355b62748c7d647382d5e47a
mescobedo <meg_544@yahoo.com.mx>
2025-11-18 15:30:23 -0600

    se modifica la vista para ordenar lista de gastos descendente

--- myapp/views.py
+++ myapp/views.py
@@ -74,7 +74,7 @@ def seleccionar_evento_listar_gastos2(request):
 
 def listar_gastos(request, evento_id):
     evento = get_object_or_404(Evento, pk=evento_id)
-    gastos = DetalleGasto.objects.filter(evento=evento)
+    gastos = DetalleGasto.objects.filter(evento=evento).order_by('-fecha')
     total_importe = gastos.aggregate(total=Sum('importe'))['total'] or 0  # Calcula la suma de los importes
     return render(request, 'listar_gastos.html', {'gastos': gastos, 'evento': evento, 'total': total_importe,})
 
